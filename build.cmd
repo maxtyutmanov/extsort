8fe9947a067ffe3e78950acd66319d46007a1006
maxtyutmanov <tyutmanovmd@gmail.com>
2020-09-03 17:43:36 +0300

    restructured the src folder

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 cd %~dp0
 dotnet publish %CD%\src\ExtSort\ExtSort.Sorter\ExtSort.Sorter.csproj -c Release --output %CD%\bin\sorter
 dotnet publish %CD%\src\ExtSort\ExtSort.Generator\ExtSort.Generator.csproj -c Release --output %CD%\bin\generator
-dotnet publish %CD%\src\ExtSort\ExtSort.EndToEndTest\ExtSort.EndToEndTest.csproj -c Release --output %CD%\bin\e2e
+dotnet publish %CD%\src\ExtSort\Tests\ExtSort.EndToEndTest\ExtSort.EndToEndTest.csproj -c Release --output %CD%\bin\e2e
